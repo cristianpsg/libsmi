46030f71a3c119f23a968dc900abacb23387ca13
strauss <strauss@ibr.cs.tu-bs.de>
1998-11-04 23:44:50 +0000

    *** empty log message ***

--- src/data.c
+++ src/data.c
@@ -8,7 +8,7 @@
  * See the file "license.terms" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * @(#) $Id: data.c,v 1.11 1998/11/02 19:29:02 strauss Exp $
+ * @(#) $Id: data.c,v 1.12 1998/11/04 02:14:54 strauss Exp $
  */
 
 #include <sys/types.h>
@@ -840,6 +840,7 @@ addMibNode(parent, subid, module, flags, parser)
     }
 
     node->module = module;
+    node->type = NULL;
     node->subid = subid;
     node->fileoffset = 0;
     node->macro = MACRO_UNKNOWN;
