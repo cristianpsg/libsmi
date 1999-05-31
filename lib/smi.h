abf1cd2de935ddc88d3b647b69b2e0e06112e033
strauss <strauss@ibr.cs.tu-bs.de>
1999-05-31 11:58:27 +0000

    *** empty log message ***

--- lib/smi.h
+++ lib/smi.h
@@ -5,21 +5,10 @@
  *
  * Copyright (c) 1999 Frank Strauss, Technical University of Braunschweig.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- * @(#) $Id: smi.h,v 1.23 1999/05/27 20:05:59 strauss Exp $
+ * @(#) $Id: smi.h,v 1.24 1999/05/30 21:50:46 strauss Exp $
  */
 
 #ifndef _SMI_H
