a9206754d2eef9b1ecc253429582420c5de99317
strauss <strauss@cbb251fc-df73-40cb-8bdd-c29527983e69>
1999-05-31 15:48:43 +0000

    *** empty log message ***

--- trunk/tools/smisubtree.c
+++ trunk/tools/smisubtree.c
@@ -8,43 +8,43 @@
  * See the file "COPYING" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * @(#) $Id: smisubtree.c,v 1.4 1999/05/25 17:00:40 strauss Exp $
+ * @(#) $Id: smisubtree.c,v 1.5 1999/05/31 11:58:43 strauss Exp $
  */
 
 #include <stdio.h>
 #include <string.h>
+#include <smi.h>
 
-#include "smi.h"
-
-int
-main(argc, argv)
-    int argc;
-    char *argv[];
+int main(int argc, char *argv[])
 {
-    char *modulename, *fullname;
-    SmiNode *smiNode, *smiStartNode;
+    char *modulename, *fullname, *oid;
+    SmiNode *smiNode;
     
     if (argc != 2) {
-	fprintf(stderr, "Usage: smisubtree oid\n");
-	exit(1);
+        fprintf(stderr, "Usage: smisubtree oid\n");
+        exit(1);
     }
-
+ 
     fullname   = argv[1];
     modulename = smiModule(fullname);
     
     smiInit();
-
+ 
     smiLoadModule(modulename);
-
-    for(smiNode = smiStartNode = smiGetNode(fullname, "");
-	smiNode && strstr(smiNode->oid, smiStartNode->oid) == smiNode->oid;
-	smiNode = smiGetNextNode(modulename, smiNode->name)) {
-
-	printf("%s::%-16s   [%s]\n",
-	       smiNode->module, smiNode->name,
-	       smiStringBasetype(smiNode->basetype));
-	
+ 
+    smiNode = smiGetNode(NULL, fullname);
+    if (smiNode) oid = smiNode->oid;
+ 
+    for(; smiNode && (strstr(smiNode->oid, oid) == smiNode->oid);
+        smiNode = smiGetNextNode(smiNode, SMI_DECL_UNKNOWN)) {
+ 
+        printf("%s.%-26s [%s]\n",
+               smiNode->module, smiNode->name, smiNode->oid);
+        
     };
     
+    smiFreeNode(smiNode);
+    free(modulename);
     exit(0);
 }
+
