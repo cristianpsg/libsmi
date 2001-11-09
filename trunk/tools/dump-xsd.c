8ba1a162dd01d3333492b83d89c80f071aba9bdf
schoenw <schoenw@cbb251fc-df73-40cb-8bdd-c29527983e69>
2001-11-09 15:29:23 +0000

    Added an initial version of the XML schema output driver.

---
+++ trunk/tools/dump-xsd.c
@@ -0,0 +1,248 @@
+/*
+ * dump-xsd.c --
+ *
+ *      Operations to dump SMIng module information as XML schemas.
+ *
+ * Copyright (c) 2001 J. Schoenwaelder, Technical University of Braunschweig.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
+ *
+ * @(#) $Id: dump-xml.c,v 1.21 2001/04/18 13:04:48 strauss Exp $
+ */
+
+#include <config.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+
+#include "smi.h"
+#include "smidump.h"
+
+
+
+#define  INDENT		2    /* indent factor */
+#define  INDENTVALUE	20   /* column to start values, except multiline */
+#define  INDENTTEXTS	4    /* column to start multiline texts */
+#define  INDENTMAX	64   /* max column to fill, break lines otherwise */
+
+
+
+typedef struct XmlEscape {
+    char character;
+    char *escape;
+} XmlEscape;
+
+static XmlEscape xmlEscapes [] = {
+    { '<',	"&lt;" },
+    { '>',	"&gt;" },
+    { '&',	"&amp;" },
+    { 0,	NULL }
+};
+
+
+static int current_column = 0;
+
+
+
+static char *getStringBasetype(SmiBasetype basetype)
+{
+    return
+        (basetype == SMI_BASETYPE_UNKNOWN)           ? "<UNKNOWN>" :
+        (basetype == SMI_BASETYPE_OCTETSTRING)       ? "OctetString" :
+        (basetype == SMI_BASETYPE_OBJECTIDENTIFIER)  ? "ObjectIdentifier" :
+        (basetype == SMI_BASETYPE_UNSIGNED32)        ? "Unsigned32" :
+        (basetype == SMI_BASETYPE_INTEGER32)         ? "Integer32" :
+        (basetype == SMI_BASETYPE_UNSIGNED64)        ? "Unsigned64" :
+        (basetype == SMI_BASETYPE_INTEGER64)         ? "Integer64" :
+        (basetype == SMI_BASETYPE_FLOAT32)           ? "Float32" :
+        (basetype == SMI_BASETYPE_FLOAT64)           ? "Float64" :
+        (basetype == SMI_BASETYPE_FLOAT128)          ? "Float128" :
+        (basetype == SMI_BASETYPE_ENUM)              ? "Enumeration" :
+        (basetype == SMI_BASETYPE_BITS)              ? "Bits" :
+                                                   "<unknown>";
+}
+
+
+
+static void fprint(FILE *f, char *fmt, ...)
+{
+    va_list ap;
+    char    s[200];
+    char    *p;
+
+    va_start(ap, fmt);
+#ifdef HAVE_VSNPRINTF
+    current_column += vsnprintf(s, sizeof(s), fmt, ap);
+#else
+    current_column += vsprintf(s, fmt, ap);	/* buffer overwrite */
+#endif
+    va_end(ap);
+
+    fputs(s, f);
+
+    if ((p = strrchr(s, '\n'))) {
+	current_column = strlen(p) - 1;
+    }
+}
+
+
+
+static void fprintSegment(FILE *f, int column, char *string, int length)
+{
+    fprint(f, "%*c%s", column, ' ', string);
+    if (length) {
+	fprint(f, "%*c", length - strlen(string) - column, ' ');
+    }
+}
+
+
+
+static void fprintMultilineString(FILE *f, int column, const char *s)
+{
+    int i, j, len;
+
+#ifdef INDENTTEXTS
+    fprintSegment(f, column + INDENTTEXTS, "", 0);
+#endif
+    if (s) {
+	len = strlen(s);
+	for (i=0; i < len; i++) {
+	    for (j = 0; xmlEscapes[j].character; j++) {
+		if (xmlEscapes[j].character == s[i]) break;
+	    }
+	    if (xmlEscapes[j].character) {
+		fputs(xmlEscapes[j].escape, f);
+		current_column += strlen(xmlEscapes[j].escape);
+	    } else {
+		putc(s[i], f);
+		current_column++;
+	    }
+	    if (s[i] == '\n') {
+		current_column = 0;
+#ifdef INDENTTEXTS
+		fprintSegment(f, column + INDENTTEXTS, "", 0);
+#endif
+	    }
+	}
+    }
+    current_column++;
+}
+
+
+
+static void fprintDocumentation(FILE *f, int indent, const char *description)
+{
+    if (description) {
+	fprintSegment(f, indent, "<xsd:documentation xml:lang=\"en\">\n", 0);
+	fprintMultilineString(f, indent, description);
+	fprint(f, "\n");
+	fprintSegment(f, indent, "</documentation>\n", 0);
+    }
+}
+
+
+
+static void fprintModule(FILE *f, SmiModule *smiModule)
+{
+    fprintSegment(f, INDENT, "<xsd:annotation>\n", 0);
+    fprintDocumentation(f, 2 * INDENT, smiModule->description);
+    fprintSegment(f, INDENT, "</xsd:annotation>\n", 0);
+}
+
+
+
+static void fprintRestriction(FILE *f, int indent, SmiType *smiType)
+{
+    fprintSegment(f, indent, "<xsd:restriction", 0);
+    fprint(f, " base=\"%s\">\n", getStringBasetype(smiType->basetype));
+    fprintSegment(f, indent, "</xsd:restriction>\n", 0);
+}
+
+
+
+static void fprintTypedef(FILE *f, int indent, SmiType *smiType)
+{
+    fprintSegment(f, indent, "<xsd:simpleType", 0);
+    if (smiType->name) {
+	fprint(f, " name=\"%s\"", smiType->name);
+    }
+    fprint(f, ">\n");
+
+    fprintRestriction(f, indent + INDENT, smiType);
+    fprintDocumentation(f, indent + INDENT, smiType->description);
+    
+    fprintSegment(f, indent, "</xsd:simpleType>\n", 0);
+}
+
+
+
+static void fprintTypedefs(FILE *f, SmiModule *smiModule)
+{
+    int		 i;
+    SmiType	 *smiType;
+    
+    for(i = 0, smiType = smiGetFirstType(smiModule);
+	smiType;
+	i++, smiType = smiGetNextType(smiType)) {
+	fprint(f, "\n");
+	fprintTypedef(f, INDENT, smiType);
+    }
+}
+
+
+
+static void dumpXsd(int modc, SmiModule **modv, int flags, char *output)
+{
+    int  i;
+    FILE *f = stdout;
+
+    if (output) {
+	f = fopen(output, "w");
+	if (!f) {
+	    fprintf(stderr, "smidump: cannot open %s for writing: ", output);
+	    perror(NULL);
+	    exit(1);
+	}
+    }
+
+    for (i = 0; i < modc; i++) {
+
+	fprint(f, "<!-- This module has been generated by smidump "
+	       SMI_VERSION_STRING ". Do not edit. -->\n");
+	fprint(f, "\n");
+	
+	fprint(f, "<xsd:schema xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\n");
+
+	fprintModule(f, modv[i]);
+	fprintTypedefs(f, modv[i]);
+
+	fprint(f, "\n</xsd:schema>\n");
+    }
+
+    if (output) {
+	fclose(f);
+    }
+}
+
+
+
+void initXsd()
+{
+    
+    static SmidumpDriver driver = {
+	"xsd",
+	dumpXsd,
+	0,
+	SMIDUMP_DRIVER_CANT_UNITE,
+	"XML schema definitions",
+	NULL,
+	NULL
+    };
+    
+    smidumpRegisterDriver(&driver);
+}
