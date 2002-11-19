59e4700f7118d77ee24f5a98bef0885d6786507d
schoenw <schoenw@ibr.cs.tu-bs.de>
2002-11-19 10:34:34 +0000

    Compute some more metrics. Added the --metrics-raw option.

--- tools/dump-metrics.c
+++ tools/dump-metrics.c
@@ -5,11 +5,12 @@
  *
  * Copyright (c) 2000 Frank Strauss, Technical University of Braunschweig.
  * Copyright (c) 2000 J. Schoenwaelder, Technical University of Braunschweig.
+ * Copyright (c) 2002 J. Schoenwaelder, University of Osnabrueck.
  *
  * See the file "COPYING" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * @(#) $Id: dump-metrics.c,v 1.7 2000/12/11 08:41:22 strauss Exp $
+ * @(#) $Id: dump-metrics.c,v 1.8 2002/10/30 09:17:37 schoenw Exp $
  */
 
 /*
@@ -29,6 +30,9 @@
 #include "smidump.h"
 
 
+static int raw = 0;
+
+
 typedef struct StatusCounter {
     unsigned int total;
     unsigned int current;
@@ -38,17 +42,73 @@ typedef struct StatusCounter {
 
 
 
+typedef struct AccessCounter {
+    unsigned int total;
+    unsigned int noaccess;
+    unsigned int notify;
+    unsigned int readonly;
+    unsigned int readwrite;
+} AccessCounter;
+
+
+
+typedef struct IndexCounter {
+    unsigned int total;
+    unsigned int index;
+    unsigned int augment;
+    unsigned int reorder;
+    unsigned int sparse;
+    unsigned int expand;
+} IndexCounter;
+
+
+
+typedef struct LengthCounter {
+    unsigned int total;
+    unsigned int description;
+    unsigned int reference;
+    unsigned int units;
+    unsigned int format;
+} LengthCounter;
+
+
+
+typedef struct FeatureCounter {
+    unsigned int total;
+    unsigned int description;
+    unsigned int reference;
+    unsigned int units;
+    unsigned int format;
+} FeatureCounter;
+
+
+
 typedef struct Metrics {
-    StatusCounter tables;
-    StatusCounter columns;
-    StatusCounter scalars;
-    StatusCounter notifications;
-    unsigned int writable;
+    StatusCounter statusTypes;
+    StatusCounter statusTables;
+    StatusCounter statusColumns;
+    StatusCounter statusScalars;
+    StatusCounter statusNotifications;
+    StatusCounter statusGroups;
+    StatusCounter statusCompliances;
+    StatusCounter statusAll;
+    AccessCounter accessColumns;
+    AccessCounter accessScalars;
+    AccessCounter accessAll;
+    IndexCounter  indexTables;
+    LengthCounter lengthTypes;
+    LengthCounter lengthTables;
+    LengthCounter lengthRows;
+    LengthCounter lengthColumns;
+    LengthCounter lengthScalars;
+    LengthCounter lengthNotifications;
+    LengthCounter lengthAll;
 } Metrics;
 
 
 
-static void incrStatusCounter(StatusCounter *cnt, SmiStatus smiStatus)
+static void
+incrStatusCounter(StatusCounter *cnt, SmiStatus smiStatus)
 {
     cnt->total++;
     switch (smiStatus) {
@@ -70,64 +130,302 @@ static void incrStatusCounter(StatusCounter *cnt, SmiStatus smiStatus)
 
 
 
-static void addMetrics(Metrics *metrics, SmiModule *smiModule)
+static void
+incrAccessCounter(AccessCounter *cnt, SmiAccess smiAccess)
 {
-    SmiNode *smiNode;
-
-    for (smiNode = smiGetFirstNode(smiModule, SMI_NODEKIND_ANY);
-	 smiNode;
-	 smiNode = smiGetNextNode(smiNode, SMI_NODEKIND_ANY)) {
-	switch (smiNode->nodekind) {
-	case SMI_NODEKIND_TABLE:
-	    incrStatusCounter(&metrics->tables, smiNode->status);
-	    break;
-	case SMI_NODEKIND_COLUMN:
-	    incrStatusCounter(&metrics->columns, smiNode->status);
-	    if (smiNode->access == SMI_ACCESS_READ_WRITE) {
-		metrics->writable++;
-	    }
-	    break;
-	case SMI_NODEKIND_SCALAR:
-	    incrStatusCounter(&metrics->scalars, smiNode->status);
-	    if (smiNode->access == SMI_ACCESS_READ_WRITE) {
-		metrics->writable++;
-	    }
-	    break;
-	case SMI_NODEKIND_NOTIFICATION:
-	    incrStatusCounter(&metrics->notifications, smiNode->status);
-	    break;
-	}
+    cnt->total++;
+    switch (smiAccess) {
+    case SMI_ACCESS_NOT_ACCESSIBLE:
+	cnt->noaccess++;
+	break;
+    case SMI_ACCESS_NOTIFY:
+	cnt->notify++;
+	break;
+    case SMI_ACCESS_READ_ONLY:
+	cnt->readonly++;
+	break;
+    case SMI_ACCESS_READ_WRITE:
+	cnt->readwrite++;
+	break;
+    case SMI_ACCESS_INSTALL:
+    case SMI_ACCESS_INSTALL_NOTIFY:
+    case SMI_ACCESS_REPORT_ONLY:
+    case SMI_ACCESS_UNKNOWN:
+    case SMI_ACCESS_NOT_IMPLEMENTED:
+	break;
     }
 }
 
 
 
-static void fprintStatusCounter(FILE *f, StatusCounter *cnt, char *s)
+static void
+incrIndexCounter(IndexCounter *cnt, SmiIndexkind indexkind)
 {
-    fprintf(f, "%-14s %3d (%3d current, %3d deprecated, %3d obsolete)\n", s,
-	    cnt->total, cnt->current, cnt->deprecated, cnt->obsolete);
+    cnt->total++;
+    switch (indexkind) {
+    case SMI_INDEX_INDEX:
+	cnt->index++;
+	break;
+    case SMI_INDEX_AUGMENT:
+	cnt->augment++;
+	break;
+    case SMI_INDEX_REORDER:
+	cnt->reorder++;
+	break;
+    case SMI_INDEX_SPARSE:
+	cnt->sparse++;
+	break;
+    case SMI_INDEX_EXPAND:
+	cnt->expand++;
+	break;
+    case SMI_INDEX_UNKNOWN:
+	break;
+    }
 }
 
 
 
-static void fprintMetrics(FILE *f, Metrics *metrics)
+static void
+incrLengthCounter(LengthCounter *cnt, char *description, char *reference,
+		  char *units, char *format)
+{
+    cnt->total++;
+    if (description) {
+	cnt->description += strlen(description);
+    }
+    if (reference) {
+	cnt->reference += strlen(reference);
+    }
+    if (units) {
+	cnt->units += strlen(units);
+    }
+    if (format) {
+	cnt->format += strlen(format);
+    }
+}
+    
+
+
+static void
+addMetrics(Metrics *metrics, SmiModule *smiModule)
+{
+    SmiNode *smiNode;
+    SmiType *smiType;
+    size_t len;
+
+    for (smiNode = smiGetFirstNode(smiModule, SMI_NODEKIND_ANY);
+	 smiNode;
+	 smiNode = smiGetNextNode(smiNode, SMI_NODEKIND_ANY)) {
+	len = smiNode->description ? strlen(smiNode->description) : 0;
+	switch (smiNode->nodekind) {
+	case SMI_NODEKIND_TABLE:
+	    incrStatusCounter(&metrics->statusTables, smiNode->status);
+	    incrStatusCounter(&metrics->statusAll, smiNode->status);
+	    incrLengthCounter(&metrics->lengthTables,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    incrLengthCounter(&metrics->lengthAll,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    break;
+	case SMI_NODEKIND_ROW:
+	    incrIndexCounter(&metrics->indexTables, smiNode->indexkind);
+	    incrLengthCounter(&metrics->lengthRows,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    incrLengthCounter(&metrics->lengthAll,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    break;
+	case SMI_NODEKIND_COLUMN:
+	    incrStatusCounter(&metrics->statusColumns, smiNode->status);
+	    incrStatusCounter(&metrics->statusAll, smiNode->status);
+	    incrAccessCounter(&metrics->accessColumns, smiNode->access);
+	    incrAccessCounter(&metrics->accessAll, smiNode->access);
+	    incrLengthCounter(&metrics->lengthColumns,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    incrLengthCounter(&metrics->lengthAll,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    break;
+	case SMI_NODEKIND_SCALAR:
+	    incrStatusCounter(&metrics->statusScalars, smiNode->status);
+	    incrStatusCounter(&metrics->statusAll, smiNode->status);
+	    incrAccessCounter(&metrics->accessScalars, smiNode->access);
+	    incrAccessCounter(&metrics->accessAll, smiNode->access);
+	    incrLengthCounter(&metrics->lengthScalars,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    incrLengthCounter(&metrics->lengthAll,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    break;
+	case SMI_NODEKIND_NOTIFICATION:
+	    incrStatusCounter(&metrics->statusNotifications, smiNode->status);
+	    incrStatusCounter(&metrics->statusAll, smiNode->status);
+	    incrLengthCounter(&metrics->lengthNotifications,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    incrLengthCounter(&metrics->lengthAll,
+			      smiNode->description, smiNode->reference,
+			      smiNode->units, smiNode->format);
+	    break;
+	case SMI_NODEKIND_GROUP:
+	    incrStatusCounter(&metrics->statusGroups, smiNode->status);
+	    incrStatusCounter(&metrics->statusAll, smiNode->status);
+	    break;
+	case SMI_NODEKIND_COMPLIANCE:
+	    incrStatusCounter(&metrics->statusCompliances, smiNode->status);
+	    incrStatusCounter(&metrics->statusAll, smiNode->status);
+	    break;
+	}
+    }
+
+    for (smiType = smiGetFirstType(smiModule);
+	 smiType;
+	 smiType = smiGetNextType(smiType)) {
+	len = smiType->description ? strlen(smiType->description) : 0;
+	incrStatusCounter(&metrics->statusTypes, smiType->status);
+	incrStatusCounter(&metrics->statusAll, smiType->status);
+	incrLengthCounter(&metrics->lengthTypes,
+			  smiType->description, smiType->reference,
+			  smiType->units, smiType->format);
+	incrLengthCounter(&metrics->lengthAll,
+			  smiType->description, smiType->reference,
+			  smiType->units, smiType->format);
+    }
+}
+
+
+
+static void
+fprintStatusCounter(FILE *f, StatusCounter *cnt, char *s)
+{
+    if (!s || !cnt) {
+	fprintf(f, "%-14s %8s %8s %11s %9s\n", "CATEGORY",
+		"TOTAL", "CURRENT", "DEPRECATED", "OBSOLETE");
+	return;
+    }
+
+    if (raw) {
+	fprintf(f, "%-14s %8u %8u %11u %9u\n", s,
+		cnt->total, cnt->current, cnt->deprecated, cnt->obsolete);
+    } else {
+	fprintf(f, "%-14s %8u %7.1f%% %10.1f%% %8.1f%%\n", s,
+		cnt->total,
+		cnt->total ? (double) cnt->current * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->deprecated * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->obsolete * 100 / cnt->total : 0);
+    }
+}
+
+
+
+static void
+fprintAccessCounter(FILE *f, AccessCounter *cnt, char *s)
+{
+    if (!s || !cnt) {
+	fprintf(f, "%-14s %8s %10s %9s %7s %8s\n", "CATEGORY",
+		"TOTAL", "READWRITE", "READONLY", "NOTIFY", "NOACCES");
+	return;
+    }
+
+    if (raw) {
+	fprintf(f, "%-14s %8u %10u %9u %7u %8u\n", s,
+		cnt->total, cnt->readwrite, cnt->readonly,
+		cnt->notify, cnt->noaccess);
+    } else {
+	fprintf(f, "%-14s %8u %9.1f%% %8.1f%% %6.1f%% %7.1f%%\n", s,
+		cnt->total,
+		cnt->total ? (double) cnt->readwrite * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->readonly * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->notify * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->noaccess * 100 / cnt->total : 0);
+    }
+}
+
+
+
+static void
+fprintIndexCounter(FILE *f, IndexCounter *cnt, char *s)
+{
+    if (! s || ! cnt) {
+	fprintf(f, "%-14s %8s %8s %8s %8s %8s %8s\n", "CATEGORY",
+		"TOTAL", "INDEX", "AUGMENT", "REORDER", "SPARSE", "EXPAND");
+	return;
+    }
+    
+    if (raw) {
+	fprintf(f, "%-14s %8u %8u %8u %8u %8u %8u\n", s,
+		cnt->total, cnt->index, cnt->augment,
+		cnt->reorder, cnt->sparse, cnt->expand);
+    } else {
+	fprintf(f, "%-14s %8u %7.1f%% %7.1f%% %7.1f%% %7.1f%% %7.1f%%\n", s,
+		cnt->total,
+		cnt->total ? (double) cnt->index * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->augment * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->reorder * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->sparse * 100 / cnt->total : 0,
+		cnt->total ? (double) cnt->expand * 100 / cnt->total : 0);
+    }
+}
+
+
+
+static void
+fprintLengthCounter(FILE *f, LengthCounter *cnt, char *s)
+{
+    if (s) {
+	fprintf(f, "%-14s %8u %12u %10u %8u %8u\n", s,
+		cnt->total, cnt->description, cnt->reference,
+		cnt->units, cnt->format);
+    } else {
+	fprintf(f, "%-14s %8s %12s %10s %8s %8s\n", "CATEGORY",
+		"TOTAL", "DESCRIPTION", "REFERENCE", "UNIT", "FORMAT");
+    }
+}
+
+
+
+static void
+fprintMetrics(FILE *f, Metrics *metrics)
 {
     unsigned int objects;
 
-    objects = metrics->columns.total + metrics->scalars.total;
-    
-    fprintStatusCounter(f, &metrics->tables, "Tables:");
-    fprintStatusCounter(f, &metrics->columns, "Columns:");
-    fprintStatusCounter(f, &metrics->scalars, "Scalars:");
-    fprintStatusCounter(f, &metrics->notifications, "Notifications:");
+    objects = metrics->statusColumns.total + metrics->statusScalars.total;
 
-    fprintf(f, "%-14s %3d (%d%%)\n", "Writable:", metrics->writable,
-	    objects ? (metrics->writable * 100 / objects) : 0);
+    fprintStatusCounter(f, NULL, NULL);
+    fprintStatusCounter(f, &metrics->statusTypes, "Types:");
+    fprintStatusCounter(f, &metrics->statusTables, "Tables:");
+    fprintStatusCounter(f, &metrics->statusColumns, "Columns:");
+    fprintStatusCounter(f, &metrics->statusScalars, "Scalars:");
+    fprintStatusCounter(f, &metrics->statusNotifications, "Notifications:");
+    fprintStatusCounter(f, &metrics->statusGroups, "Groups:");
+    fprintStatusCounter(f, &metrics->statusCompliances, "Compliances:");
+    fprintStatusCounter(f, &metrics->statusAll, "Summary:");
+    fprintf(f, "\n");
+    fprintAccessCounter(f, NULL, NULL);
+    fprintAccessCounter(f, &metrics->accessColumns, "Columns:");
+    fprintAccessCounter(f, &metrics->accessScalars, "Scalars:");
+    fprintAccessCounter(f, &metrics->accessAll, "Summary:");
+    fprintf(f, "\n");
+    fprintIndexCounter(f, NULL, NULL);
+    fprintIndexCounter(f, &metrics->indexTables, "Tables:");
+    fprintf(f, "\n");
+    fprintLengthCounter(f, NULL, NULL);
+    fprintLengthCounter(f, &metrics->lengthTypes, "Types:");
+    fprintLengthCounter(f, &metrics->lengthTables, "Tables:");
+    fprintLengthCounter(f, &metrics->lengthColumns, "Columns:");
+    fprintLengthCounter(f, &metrics->lengthScalars, "Scalars:");
+    fprintLengthCounter(f, &metrics->lengthNotifications, "Notifications:");
+    fprintLengthCounter(f, &metrics->lengthAll, "Summary:");
 }
 
 
 
-static void dumpMetrics(int modc, SmiModule **modv, int flags, char *output)
+static void
+dumpMetrics(int modc, SmiModule **modv, int flags, char *output)
 {
     Metrics   metrics;
     int       i;
@@ -150,7 +448,9 @@ static void dumpMetrics(int modc, SmiModule **modv, int flags, char *output)
 	for (i = 0; i < modc; i++) {
 	    memset(&metrics, 0, sizeof(Metrics));
 	}
-	addMetrics(&metrics, modv[i]);
+	for (i = 0; i < modc; i++) {
+	    addMetrics(&metrics, modv[i]);
+	}
 	fprintMetrics(f, &metrics);
     } else {
 	for (i = 0; i < modc; i++) {
@@ -176,16 +476,22 @@ static void dumpMetrics(int modc, SmiModule **modv, int flags, char *output)
 
 
 
-void initMetrics()
+void
+initMetrics()
 {
-    
+    static SmidumpDriverOption opt[] = {
+	{ "raw", OPT_FLAG, &raw, 0,
+	  "generate raw statistics (no percentages)"},
+        { 0, OPT_END, 0, 0 }
+    };
+
     static SmidumpDriver driver = {
 	"metrics",
 	dumpMetrics,
-	SMI_FLAG_NODESCR,
+	0,
 	0,
 	"metrics characterizing MIB modules",
-	NULL,
+	opt,
 	NULL
     };
     
