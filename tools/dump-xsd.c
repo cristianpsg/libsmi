fa9ef95c5f33641e3e3083b0a1e02756c6e5dfee
tklie <tklie@cbb251fc-df73-40cb-8bdd-c29527983e69>
2003-07-11 06:13:03 +0000

    fixed typo in lengths tag

--- tools/dump-xsd.c
+++ tools/dump-xsd.c
@@ -10,7 +10,7 @@
  * See the file "COPYING" for information on usage and redistribution
  * of this file, and for a DISCLAIMER OF ALL WARRANTIES.
  *
- * @(#) $Id: dump-xsd.c,v 1.76 2003/05/16 14:42:34 tklie Exp $
+ * @(#) $Id: dump-xsd.c,v 1.77 2003/07/04 15:10:20 tklie Exp $
  */
 
 #include <config.h>
@@ -1301,32 +1301,30 @@ static void fprintDisplayHint( FILE *f, char *format )
 
 static void fprintLengths( FILE *f, SmiType *smiType )
 {
-  SmiRange *smiRange = smiGetFirstRange( smiType );
-  unsigned int numSubRanges = getNumSubRanges( smiType ),
-    lp = 0;
-  SmiUnsigned32 *lengths = xmalloc(  2 * numSubRanges * 4 );
-
-  /* write subtype lengths to the array */
-  for( smiRange = smiGetFirstRange( smiType );
-       smiRange;
-       smiRange = smiGetNextRange( smiRange ) ) {
-    lengths[ lp++ ] = smiRange->minValue.value.unsigned32;
-    lengths[ lp++ ] = smiRange->maxValue.value.unsigned32;
-  }
-
-  if( numSubRanges ) {
-    fprintSegment( f, 1, "<lenghts>\n" );
-  }
-  
-  for( lp = 0; lp < numSubRanges * 2; lp = lp + 2 ) {
-    fprintSegment( f, 0, "<length min=\"%u\" max=\"%u\"/>\n", lengths[ lp ],
-		   lengths[ lp + 1 ] );
-  } 
-  
-  if( numSubRanges ) {
-    fprintSegment( f, -1, "</lengths>\n" );
-    xfree( lengths );
-  }
+    SmiRange *smiRange = smiGetFirstRange( smiType );
+    unsigned int numSubRanges = getNumSubRanges( smiType ),
+	lp = 0;
+    SmiUnsigned32 *lengths = xmalloc(  2 * numSubRanges * 4 );
+    
+    /* write lengths to the array */
+    for( smiRange = smiGetFirstRange( smiType );
+	 smiRange;
+	 smiRange = smiGetNextRange( smiRange ) ) {
+	lengths[ lp++ ] = smiRange->minValue.value.unsigned32;
+	lengths[ lp++ ] = smiRange->maxValue.value.unsigned32;
+    }
+    
+    if( numSubRanges ) {
+	fprintSegment( f, 1, "<lengths>\n" );
+    }
+    for( lp = 0; lp < numSubRanges * 2; lp = lp + 2 ) {
+	fprintSegment( f, 0, "<length min=\"%u\" max=\"%u\"/>\n",
+		       lengths[ lp ], lengths[ lp + 1 ] );
+    } 
+    if( numSubRanges ) {
+	fprintSegment( f, -1, "</lengths>\n" );
+	xfree( lengths );
+    }
 }
 
 
