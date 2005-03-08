9132a4bb6525dac255ba2b7c34651acdb92f8064
sperner <sperner@cbb251fc-df73-40cb-8bdd-c29527983e69>
2005-03-08 16:15:49 +0000

    changed printing of date: e.g. 1996-02-28 21:55 instead of 199602282155Z

--- tools/dump-svg.c
+++ tools/dump-svg.c
@@ -217,9 +217,14 @@ static char *getTimeString(time_t t)
     if (s) xfree(s);
     
     tm = gmtime(&t);
-    smiAsprintf(&s, "%04d%02d%02d%02d%02dZ",
-		tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
-		tm->tm_hour, tm->tm_min);
+    if (tm->tm_hour == 0 && tm->tm_min == 0) {
+	smiAsprintf(&s, "%04d-%02d-%02d",
+			tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday);
+    } else {
+	smiAsprintf(&s, "%04d-%02d-%02d %02d:%02d",
+			tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
+			tm->tm_hour, tm->tm_min);
+    }
     return s;
 }
 
