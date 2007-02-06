01d26d60c7f6b62b0b75fa35d2e272df914c0c44
schoenw <schoenw@cbb251fc-df73-40cb-8bdd-c29527983e69>
2007-02-06 13:08:56 +0000

    Changed suggested by Andrew Hood <ajhood@fl.net.au> to make builds on Win32 using MSVC2005 easier.

--- trunk/tools/rea.c
+++ trunk/tools/rea.c
@@ -72,7 +72,7 @@ int       SHOW_DEPR_OBSOLETE    = 0; /* false, show deprecated and
 int       STATIC_OUTPUT         = 0; /* false, enable interactivity */
 //variables for cm-driver
 int       XPLAIN                = 0; /* false, generates ASCII output */
-int       DEBUG                 = 0; /* false, generates additional
+int       XPLAIN                 = 0; /* false, generates additional
 					       output in xplain-mode */
 int       SUPPRESS_DEPRECATED   = 1; /* true, suppresses deprecated
 					       objects */
@@ -470,7 +470,7 @@ static void graphShowEdges(Graph *graph)
   
     for (tEdge = graph->edges; tEdge; tEdge = tEdge->nextPtr) {
 
-	if (DEBUG) {
+	if (XPLAIN) {
 	    switch (tEdge->enhancedindex) {
 	    case GRAPH_ENHINDEX_UNKNOWN :
 		printf("[UNKNOWN] ");
